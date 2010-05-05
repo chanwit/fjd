37e6b02a0369479e1d94520a04cbbe350dbb0aa5
Chanwit Kaewkasi <chanwit@gmail.com>
2010-05-05 21:13:17 +0700

    refactored to use .groovy rather than .java

---
+++ src/main/groovy/org/fjd/compiler/G7ASTTransform.groovy
@@ -0,0 +1,34 @@
+package org.fjd.compiler
+
+import org.codehaus.groovy.transform.ASTTransformation
+import org.codehaus.groovy.transform.GroovyASTTransformation
+import org.objectweb.asm.Opcodes
+
+import org.codehaus.groovy.ast.*
+
+import org.codehaus.groovy.control.CompilePhase
+import org.codehaus.groovy.control.SourceUnit
+
+import java.util.List
+import java.util.LinkedList
+
+import org.mbte.groovypp.compiler.*
+
+@Typed
+@GroovyASTTransformation(phase = CompilePhase.CONVERSION)
+class G7ASTTransform implements ASTTransformation, Opcodes {
+
+    void visit(ASTNode[] nodes, SourceUnit source) {
+        ModuleNode module = nodes[0]
+        def toProcess     = new LinkedList<ClassNode>()
+        final fileName    = source.name
+        final forceTyped  = fileName.endsWith(".fjd") || fileName.endsWith(".g7")
+
+        if(forceTyped) {
+            for(c in module.classes) {
+                c.addAnnotation(new AnnotationNode(TypeUtil.TYPED))
+            }
+        }
+    }
+
+}
